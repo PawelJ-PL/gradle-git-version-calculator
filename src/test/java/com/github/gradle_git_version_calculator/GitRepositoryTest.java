bca522f773b96b2302259199df659fa352c90ae8
Pawel <inne.poczta@gmail.com>
2017-10-28 23:18:32 +0200

    unit tests

--- src/test/java/com/github/gradle_git_version_calculator/GitRepositoryTest.java
+++ src/test/java/com/github/gradle_git_version_calculator/GitRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.github.gradle_git_version_calculator;
 
+import com.github.gradle_git_version_calculator.Exceptions.NoNamesFoundError;
+import com.github.gradle_git_version_calculator.Exceptions.NotGitRepositoryError;
 import com.github.gradle_git_version_calculator.git_commands.DescribeCommand;
 import com.github.gradle_git_version_calculator.models.GitCommandResult;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -52,6 +55,38 @@ public class GitRepositoryTest {
         //then
         assertThat(result.isPresent()).isTrue();
         assertThat(result.get()).isEqualTo("1.0.0");
+        verify(describeCommand, times(1)).setTags(true);
+        verify(describeCommand, times(1)).setAbbrev(0);
+    }
+    
+    @Test
+    public void shouldReturnEmptyOnNotRepositoryError() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(128, Collections.emptyList(),
+                                                              Collections.singletonList("Not git repository"));
+        when(describeCommand.call()).thenThrow(new NotGitRepositoryError(commandResult));
+        
+        //when
+        Optional<String> result = gitRepository.getLatestTag();
+        
+        //then
+        assertThat(result.isPresent()).isFalse();
+        verify(describeCommand, times(1)).setTags(true);
+        verify(describeCommand, times(1)).setAbbrev(0);
+    }
+    
+    @Test
+    public void shouldReturnEmptyOnNoNamesFoundError() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(128, Collections.emptyList(),
+                                                              Collections.singletonList("Not names found"));
+        when(describeCommand.call()).thenThrow(new NoNamesFoundError(commandResult));
+        
+        //when
+        Optional<String> result = gitRepository.getLatestTag();
+        
+        //then
+        assertThat(result.isPresent()).isFalse();
         verify(describeCommand, times(1)).setTags(true);
         verify(describeCommand, times(1)).setAbbrev(0);
     }
