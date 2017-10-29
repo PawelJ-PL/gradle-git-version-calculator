1ef49815e805ecce8f6dc14d856ca49795179b8d
Pawel <inne.poczta@gmail.com>
2017-10-29 21:06:20 +0100

    unit tests

--- src/main/java/com/github/gradle_git_version_calculator/GitRepository.java
+++ src/main/java/com/github/gradle_git_version_calculator/GitRepository.java
@@ -24,17 +24,14 @@ public class GitRepository {
             //TODO add logger here
             return Optional.empty();
         }
-        if (result.getStdOut().size() != 1) {
-            throw new RuntimeException(String.format("Expected 1 line, got %d:\n%s",
-                                                     result.getStdOut().size(),
-                                                     result.getStdOutAsString()));
-        }
+        verifySingleOutputLine(result);
         return Optional.of(result.getStdOutAsString().trim());
     }
     
     public Integer getCommitsSinceTag(String tag) {
         GitCommand command = commandsFactory.getRevList().setSpec(String.format("%s..", tag)).setCount(true);
         GitCommandResult result = command.call();
+        verifySingleOutputLine(result);
         return Integer.parseInt(result.getStdOutAsString().trim());
     }
     
@@ -42,6 +39,14 @@ public class GitRepository {
         GitCommand command = commandsFactory.getStatus().setPorcelain(true);
         GitCommandResult result = command.call();
         return (result.getStdOut().size() == 0);
+    }
+    
+    private void verifySingleOutputLine(GitCommandResult result) {
+        if (result.getStdOut().size() != 1) {
+            throw new RuntimeException(String.format("Expected 1 line, got %d:\n%s",
+                                                     result.getStdOut().size(),
+                                                     result.getStdOutAsString()));
+        }
     }
     
 }
--- src/test/java/com/github/gradle_git_version_calculator/GitRepositoryTest.java
+++ src/test/java/com/github/gradle_git_version_calculator/GitRepositoryTest.java
@@ -3,9 +3,13 @@ package com.github.gradle_git_version_calculator;
 import com.github.gradle_git_version_calculator.Exceptions.NoNamesFoundError;
 import com.github.gradle_git_version_calculator.Exceptions.NotGitRepositoryError;
 import com.github.gradle_git_version_calculator.git_commands.DescribeCommand;
+import com.github.gradle_git_version_calculator.git_commands.RevListCommand;
+import com.github.gradle_git_version_calculator.git_commands.StatusCommand;
 import com.github.gradle_git_version_calculator.models.GitCommandResult;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,6 +17,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -23,6 +28,11 @@ public class GitRepositoryTest {
     
     private GitRepository gitRepository;
     private DescribeCommand describeCommand;
+    private RevListCommand revListCommand;
+    private StatusCommand statusCommand;
+    
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
     
     @Before
     public void setUp() {
@@ -34,12 +44,27 @@ public class GitRepositoryTest {
         describeCommand = mock(DescribeCommand.class);
         when(gitCommandsFactory.getDescribe()).thenReturn(describeCommand);
         setupDescribeCommand();
+        revListCommand = mock(RevListCommand.class);
+        when(gitCommandsFactory.getRevList()).thenReturn(revListCommand);
+        setupRevListCommand();
+        statusCommand = mock(StatusCommand.class);
+        when(gitCommandsFactory.getStatus()).thenReturn(statusCommand);
+        setupStatusCommand();
         return gitCommandsFactory;
     }
     
     private void setupDescribeCommand() {
         when(describeCommand.setTags(anyBoolean())).thenReturn(describeCommand);
         when(describeCommand.setAbbrev(anyInt())).thenReturn(describeCommand);
+    }
+    
+    private void setupRevListCommand() {
+        when(revListCommand.setCount(anyBoolean())).thenReturn(revListCommand);
+        when(revListCommand.setSpec(anyString())).thenReturn(revListCommand);
+    }
+    
+    private void setupStatusCommand() {
+        when(statusCommand.setPorcelain(anyBoolean())).thenReturn(statusCommand);
     }
     
     @Test
@@ -89,6 +114,120 @@ public class GitRepositoryTest {
         assertThat(result.isPresent()).isFalse();
         verify(describeCommand, times(1)).setTags(true);
         verify(describeCommand, times(1)).setAbbrev(0);
+    }
+    
+    @Test
+    public void shouldThrowExceptionWhenDescribeOutputMoreOutputLines() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Arrays.asList("First line", "second line"),
+                                                              Collections.emptyList());
+        when(describeCommand.call()).thenReturn(commandResult);
+        
+        //when
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Expected 1 line, got 2:\nFirst line\nsecond line");
+        gitRepository.getLatestTag();
+        
+        //then
+        verify(describeCommand, times(1)).setTags(true);
+        verify(describeCommand, times(1)).setAbbrev(0);
+    }
+    
+    @Test
+    public void shouldThrowExceptionWhenDsecribeOutputNoOutputLine() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Collections.emptyList(),
+                                                              Collections.emptyList());
+        when(describeCommand.call()).thenReturn(commandResult);
+        
+        //when
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Expected 1 line, got 0:");
+        gitRepository.getLatestTag();
+        
+        //then
+        verify(describeCommand, times(1)).setTags(true);
+        verify(describeCommand, times(1)).setAbbrev(0);
+    }
+    
+    @Test
+    public void shouldGetNumberOfCommitsSinceTag() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Collections.singletonList("7"),
+                                                              Collections.emptyList());
+        when(revListCommand.call()).thenReturn(commandResult);
+        
+        //when
+        Integer result = gitRepository.getCommitsSinceTag("0.1.0");
+        
+        //then
+        assertThat(result).isEqualTo(7);
+        verify(revListCommand, times(1)).setCount(true);
+        verify(revListCommand, times(1)).setSpec("0.1.0..");
+    }
+    
+    @Test
+    public void shouldThrowErrorWhenRevListOutputMultipleLine() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Arrays.asList("7", "10"),
+                                                              Collections.emptyList());
+        when(revListCommand.call()).thenReturn(commandResult);
+        
+        //when
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Expected 1 line, got 2:\n7\n10");
+        gitRepository.getCommitsSinceTag("0.1.0");
+        
+        //then
+        verify(revListCommand, times(1)).setCount(true);
+        verify(revListCommand, times(1)).setSpec("0.1.0..");
+    }
+    
+    @Test
+    public void shouldThrowErrorWhenRevListOutputNoLines() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Collections.emptyList(),
+                                                              Collections.emptyList());
+        when(revListCommand.call()).thenReturn(commandResult);
+        
+        //when
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Expected 1 line, got 0:");
+        gitRepository.getCommitsSinceTag("0.1.0");
+        
+        //then
+        verify(revListCommand, times(1)).setCount(true);
+        verify(revListCommand, times(1)).setSpec("0.1.0..");
+    }
+    
+    @Test
+    public void shouldReturnTrueOnCleanRepository() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Collections.emptyList(),
+                                                              Collections.emptyList());
+        when(statusCommand.call()).thenReturn(commandResult);
+        
+        //when
+        Boolean result = gitRepository.isClean();
+        
+        //then
+        assertThat(result).isTrue();
+        verify(statusCommand, times(1)).setPorcelain(true);
+    }
+    
+    @Test
+    public void shouldReturnFalseOnDirtyRepository() {
+        //given
+        GitCommandResult commandResult = new GitCommandResult(0, Collections.singletonList("??"),
+                                                              Collections.emptyList());
+        when(statusCommand.call()).thenReturn(commandResult);
+        
+        //when
+        Boolean result = gitRepository.isClean();
+        
+        //then
+        assertThat(result).isFalse();
+        verify(statusCommand, times(1)).setPorcelain(true);
     }
     
 }
